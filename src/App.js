edbf963467b4c0f4e5e3b23ae41269d1bf0112bc
Evgeniya <eva147@list.ru>
2022-07-12 14:23:38 +0100

    Add react-router-dom@6 and update the code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { BrowserRouter, Switch, Router } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
 // page components
 import Home from './pages/home/Home'
-import Create from './pages/search/Search'
+import Create from './pages/create/Create'
 import Recipe from './pages/recipe/Recipe'
 import Search from './pages/search/Search'
 
@@ -14,22 +14,12 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Router exact path='/'>
-            <Home />
-          </Router>
-          <Router path='/create'>
-            <Create />
-          </Router>
-          <Router path='/search'>
-            <Search />
-          </Router>
-          <Router path='/recipes/:id'>
-            <Recipe />
-          </Router>
-        </Switch>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/recipe" element={<Recipe />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
     </div>
   );
 }
