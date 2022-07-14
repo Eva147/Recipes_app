9ace18ea1707bff50f1415ce5789a77bac2b1cc1
Evgeniya <eva147@list.ru>
2022-07-14 20:32:33 +0100

    Add main icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import {NavLink} from 'react-router-dom'
 import React from 'react'
+import SoupKitchenIcon from '@mui/icons-material/SoupKitchen'
 
 // styles
 import './Navbar.css'
@@ -8,6 +9,7 @@ export default function Navbar() {
   return (
     <div className='navbar'>
         <nav>
+            <SoupKitchenIcon className='icon'/>
             <NavLink to='/' className='brand'><h1>Cooking with Eugenie</h1></NavLink>
             <NavLink to='/create'><h1>Create recipe</h1></NavLink>
         </nav>
