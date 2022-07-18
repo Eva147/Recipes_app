19f4c4c37a7c0a91d84fdaf20b6b689a11c8b261
Evgeniya <eva147@list.ru>
2022-07-18 10:37:02 +0100

    Add ingredients into Create component

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {useState} from 'react'
+import {useState, useRef} from 'react'
 // styles
 import './Create.css'
 
@@ -8,10 +8,28 @@ export default function Create() {
     const [title, setTitle] = useState('')
     const [method, setMethod] = useState('')
     const [cookingTime, setCookingTime] = useState('')
+    const [newIngredient, setNewIngredient] = useState('')
+    const [ingredients, setNewIngredients] = useState([])
+    // create a ref to focus on the input for ings
+    const ingredientInput = useRef(null)
 
-    const handleSubmit = (e) => {
+    const handleSubmit = e => {
         e.preventDefault()
-        console.log(title, method, cookingTime)
+        console.log(ingredients)
+    }
+
+    const handleAdd = e => {
+        e.preventDefault()
+        // trim() take away whitespace from ingredient
+        const ing = newIngredient.trim()  
+        // if there are ingredients in the array and there is no this new ingredient, then add it to the array
+        if (ing && !ingredients.includes(ing)) {
+            setNewIngredients(prevIngrediens => [...prevIngrediens, ing])
+        }
+        // empty the input for a new ingredient
+        setNewIngredient('')
+        // use focus method to focus the input field (the cursor is there while user add ings)
+        ingredientInput.current.focus()
     }
 
     return (
@@ -28,6 +46,22 @@ export default function Create() {
                         required
                     />
                 </label>
+
+                <label>
+                    <span>Recipe ingredients:</span>
+                    <div className='ingredients'>
+                        <input 
+                            type='text'
+                            onChange={e => setNewIngredient(e.target.value)}
+                            value={newIngredient}
+                            // focus input to not to click on the input every time when add ings
+                            ref={ingredientInput}
+                        />    
+                        <button onClick={handleAdd} className='btn'>add</button>
+                    </div>
+                </label>
+                <p>Current ingredients:{ingredients.map(i => <em key={i}>{i}, </em>)}</p>
+
                 <label>
                     <span>Recipe method:</span>
                     <textarea 
